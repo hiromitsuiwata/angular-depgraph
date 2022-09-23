9d37e46dd2e21f9c0a91f77f22f28d8110ca43bb
Hiromitsu Iwata <hiromitsu7@gmail.com>
2022-09-24 00:56:25 +0900

    hierarchy

--- src/index.ts
+++ src/index.ts
@@ -9,58 +9,63 @@ export class Parser {
     const program = ts.createProgram(filePaths, compilerOptions);
 
     const sourceFiles = program.getSourceFiles();
-
     sourceFiles.forEach((sourceFile) => {
-      if (sourceFile.fileName.includes('node_modules')) {
+      if (!filePaths.includes(sourceFile.fileName)) {
         return;
       }
       console.log('sourceFile:' + sourceFile.fileName);
-      this.printAllChildren(sourceFile);
-      console.log('#####################################');
+
+      const hierarchy: string[] = [];
+
+      this.printAllChildren(sourceFile, 0, hierarchy);
+      console.log('=====================================');
     });
   }
 
-  private printAllChildren(node: ts.Node, depth = 0) {
-    console.log('[node] pos:', node.pos, node.end);
-    console.log('[node] SyntaxKind:', ts.SyntaxKind[node.kind]);
-
-    if (ts.canHaveModifiers(node) && ts.getModifiers(node)) {
-      console.log('[node] modifiers:', ts.getModifiers(node));
+  private printAllChildren(node: ts.Node, depth: number, hierarchy: string[]) {
+    this.print(depth, '[Node] pos:', node.pos, node.end);
+    this.print(depth, '[Node] kind:', this.getKind(node));
+    if (hierarchy.length > depth) {
+      hierarchy = hierarchy.slice(0, depth);
     }
-    if (ts.canHaveDecorators(node) && ts.getDecorators(node)) {
-      console.log('[node] decorators:', ts.getDecorators(node));
-      ts.getDecorators(node)?.forEach((decorator) => {
-        console.log('[node] decorator:', decorator);
-      });
-    }
-
-    if (ts.isImportDeclaration(node)) {
-      if (node.importClause) {
-        console.log('[import] importClause:', node.importClause);
-      }
-    }
+    hierarchy[depth] = this.getKind(node);
+    this.print(depth, '[Node] hierarchy:', hierarchy.join('.'));
 
     if (ts.isClassDeclaration(node)) {
-      console.log('[class] name:', node.name);
-      console.log('[class] members:', node.members);
+      this.print(
+        depth,
+        '[ClassDeclaration] name?.escapedText:',
+        node.name?.escapedText
+      );
     }
 
-    if (node.getSourceFile()) {
-      console.log('[node] text:', node.getText());
-      console.log('-------------------------------------');
-    } else {
-      console.log('-------------------------------------');
-      return;
+    if (ts.isStringLiteral(node)) {
+      this.print(depth, '[StringLiteral] text:', node.text);
     }
+
+    if (ts.isIdentifier(node)) {
+      this.print(depth, '[Identifier] escapedText:', node.escapedText);
+    }
+
+    this.print(depth, '#####################################');
 
     depth++;
 
-    try {
-      node.getChildren().forEach((c) => {
-        this.printAllChildren(c, depth);
-      });
-    } catch (e) {
-      console.log(e);
+    ts.forEachChild(node, (childNode: ts.Node) => {
+      this.printAllChildren(childNode, depth, hierarchy);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private print(depth: number, message: any, ...optional: any[]): void {
+    let indent = '';
+    for (let i = 0; i < depth; i++) {
+      indent += '--';
     }
+    console.log(indent, message, optional);
+  }
+
+  private getKind(node: ts.Node): string {
+    return ts.SyntaxKind[node.kind].toString();
   }
 }
